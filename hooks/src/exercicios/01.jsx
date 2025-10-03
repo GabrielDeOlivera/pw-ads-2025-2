881e12240ac2f907a2369000f3b53d728f4c5d55
Gabriel de Oliveira Martins <1090482411041@Fatec-srv.local>
2025-10-03 09:11:02 -0300

    (03/10) React Hooks: exercício 01

--- hooks/src/exercicios/01.jsx
+++ hooks/src/exercicios/01.jsx
@@ -2,25 +2,33 @@ import * as React from 'react'
 
 function Greeting({initialName}) {
   // 💣 exclua esta declaração de variável e troque por uma chamada a React.useState
-  const name = ''
+  //const name = ''
+  const [name, setName] = React.useState(initialName) // valor inicial da variavel 
 
   function handleChange(event) {
     // 🐨 atualize o nome aqui baseado em event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Nome: </label>
-        <input onChange={handleChange} id="name" />
+        {/*em react componente controlado e aquele que tem seu valor sincornizado com a variavel de estado 
+        assim quando a variavel de estado ,muda seu valor seu valor, o valor do componente tambem muda o inverso tambem ocorre 
+        se o valor do componente muda o valor da variavel de estado tambem e mudado ou alterado 
+        no caso abaixo isso e implementado associado o valor do atributo "value" a variavel de estado */}
+
+
+        <input onChange={handleChange} id="name" value = {name} />
       </form>
-      {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
+      {name ? <strong>Olá{name}</strong> : 'Por favor, informe seu nome'}
     </div>
   )
 }
 
-function App() {
+function Exercicio01() {
   return Greeting({initialName: "Turíbio"})
 }
 
-export default App
+export default Exercicio01
